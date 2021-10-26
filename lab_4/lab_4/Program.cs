90a996f1cd317520a0a478078648146c1b3b3e9c
nikitaantsev <71016018+nikitaantsev@users.noreply.github.com>
2021-10-26 03:23:10 +0300

    Add files via upload

---
+++ lab_4/lab_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Журнал журнал = new Журнал { Vidacha = "20.01.2001", Vipysk = "4" };
+            
+            Izdanie[] data = { журнал };
+
+            PrintPersons(журнал);
+        }
+
+        static void PrintPersons(Izdanie[] журнал)
+        {
+            foreach (var person in журнал)
+            {
+                person.PrintFullName();
+            }    
+        }
+    }
+}
